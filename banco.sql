49ab3d52c62053ec2ab7c58e0994e069bcc53364
Vítor F. <135766335+vitorfranz@users.noreply.github.com>
2024-08-14 22:34:30 -0300

    banco.sql

--- banco.sql
+++ banco.sql
@@ -1,115 +1,110 @@
-
--- Tabela Pessoa
-create table Pessoa(
-	ID_Pessoa SERIAL primary key,
-	CPF varchar(14),
-	Matricula varchar(9),
-	Nome varchar(50),
-	Sobrenome varchar(50),
-	Data_Nascimento date,
-	CNH varchar(11),
-	Celular varchar(17),
-	Motorista BOOLEAN,
-	Email varchar(50),
-	Senha varchar(50)
+-- Table User
+create table User(
+    ID_User SERIAL primary key,
+    CPF varchar(14),
+    Registration varchar(9),
+    Name varchar(50),
+    Surname varchar(50),
+    BirthDate date,
+    DriverLicense varchar(11),
+    Phone varchar(17),
+    UserType BOOLEAN,
+    institutionalEmail varchar(50),
+    Password varchar(50),
+		registration varchar(50)
 );
 
-
--- Dias idos
-create table Dias_Aula(
-	ID_Dias SERIAL primary key,
-	ID_Pessoa SERIAL,
-	Segunda BOOLEAN,
-	Terca BOOLEAN,
-	Quarta BOOLEAN,
-	Quinta BOOLEAN,
-	Sexta BOOLEAN,
-	Sabado BOOLEAN,
-	FOREIGN KEY (ID_Pessoa) REFERENCES Pessoa(ID_Pessoa)
+-- Table ClassDays
+create table ClassDays(
+    ID_Days SERIAL primary key,
+    ID_User SERIAL,
+    Monday BOOLEAN,
+    Tuesday BOOLEAN,
+    Wednesday BOOLEAN,
+    Thursday BOOLEAN,
+    Friday BOOLEAN,
+    Saturday BOOLEAN,
+    FOREIGN KEY (ID_User) REFERENCES User(ID_User)
 );
 
-
--- Tabela Endereco
-create table Endereco(
-	ID_Endereco SERIAL primary key,
-	ID_Pessoa SERIAL,
-	CEP varchar(9),
-	Logradouro varchar(100),
-	Bairro varchar(50),
-	Cidade varchar(50),
-	Numero numeric(5),
-	Complemento varchar(50),
-	Ponto_Referencia varchar(50),
-	Estado varchar(50),
-	FOREIGN KEY (ID_Pessoa) REFERENCES Pessoa(ID_Pessoa)
+-- Table Address
+create table Address(
+    ID_Address SERIAL primary key,
+    ID_User SERIAL,
+    ZipCode varchar(9),
+    Street varchar(100),
+    Neighborhood varchar(50),
+    City varchar(50),
+    Number numeric(5),
+    AdditionalInfo varchar(50),
+    ReferencePoint varchar(50),
+    State varchar(50),
+    FOREIGN KEY (ID_User) REFERENCES User(ID_User)
 );
 
-
-
-
--- Tabela Carro
-create table Carro(
-	ID_Carro SERIAL primary key,
-	ID_Pessoa SERIAL,
-	Fabricacao Date,
-	ano_fabricacao Date,
-	ano_modelo Date,
-	Cor varchar(45),
-	Marca varchar(45),
-	Modelo varchar(45),
-	Placa varchar(7),
-	FOREIGN KEY (ID_Pessoa) REFERENCES Pessoa(ID_Pessoa)
+-- Table vehicle
+create table vehicle(
+    ID_vehicle SERIAL primary key,
+    ID_User SERIAL,
+    ManufacturingDate Date,
+    ManufacturingYear Date,
+    ModelYear Date,
+    Color varchar(45),
+    Brand varchar(45),
+    Model varchar(45),
+    Plate varchar(7),
+		City varchar(45),
+		State varchar(45),
+    FOREIGN KEY (ID_User) REFERENCES User(ID_User)
 );
 
-
--- Tabela Rota
-create table Rota(
-	ID_Rota SERIAL primary key,
-	ID_Pessoa SERIAL,
-	Nome_Rota varchar(50),
-	FOREIGN KEY (ID_Pessoa) REFERENCES Pessoa(ID_Pessoa)
+-- Table Route
+create table Route(
+    ID_Route SERIAL primary key,
+    ID_User SERIAL,
+    RouteName varchar(50),
+    FOREIGN KEY (ID_User) REFERENCES User(ID_User)
 );
 
--- Match
-create table Relacionameto(
-	ID_Relacionameto SERIAL primary key,
-	Motorista_ID SERIAL,
-	Caroneiro_ID SERIAL,
-	Valor NUMERIC(10, 2),
-	FOREIGN KEY (Motorista_ID) REFERENCES Pessoa(ID_Pessoa),
-	FOREIGN KEY (Caroneiro_ID) REFERENCES Pessoa(ID_Pessoa)
+-- Table Relationship (Match)
+create table Relationship(
+    ID_Relationship SERIAL primary key,
+    Driver_ID SERIAL,
+    Rider_ID SERIAL,
+    Amount NUMERIC(10, 2),
+    FOREIGN KEY (Driver_ID) REFERENCES User(ID_User),
+    FOREIGN KEY (Rider_ID) REFERENCES User(ID_User)
 );
 
--- Tabela Corrida
-create table Corrida(
-	ID_Corrida SERIAL primary key,
-	ID_Relacionameto SERIAL,
-	Dta_Realizacao date,
-	FOREIGN KEY (ID_Relacionameto) REFERENCES Relacionameto(ID_Relacionameto)
+-- Table Ride
+create table Ride(
+    ID_Ride SERIAL primary key,
+    ID_Relationship SERIAL,
+    DateOfRide date,
+    FOREIGN KEY (ID_Relationship) REFERENCES Relationship(ID_Relationship)
 );
 
--- Tabela Agendamento
-create table Agendamento(
-	ID_Agendamento SERIAL primary key,
-	ID_Relacionameto SERIAL,
-	Data_Agendamento date DEFAULT CURRENT_DATE,
-	FOREIGN KEY (ID_Relacionameto) REFERENCES Relacionameto(ID_Relacionameto)
+-- Table Booking
+create table Booking(
+    ID_Booking SERIAL primary key,
+    ID_Relationship SERIAL,
+    BookingDate date DEFAULT CURRENT_DATE,
+    FOREIGN KEY (ID_Relationship) REFERENCES Relationship(ID_Relationship)
 );
 
-
-create table Devido(
-	ID_Divida SERIAL primary key,
-	ID_Relacionameto SERIAL,
-	valor NUMERIC(10, 2),
-	FOREIGN KEY (ID_Relacionameto) REFERENCES Relacionameto(ID_Relacionameto)
+-- Table Debt
+create table Debt(
+    ID_Debt SERIAL primary key,
+    ID_Relationship SERIAL,
+    Amount NUMERIC(10, 2),
+    FOREIGN KEY (ID_Relationship) REFERENCES Relationship(ID_Relationship)
 );
 
-
--- Tabela Pontos_Rota
-create table Pontos_Rota(
-	ID_Pontos_Rotas SERIAL primary key,
-	ID_Rota SERIAL,
-	Longitude varchar(50),
-	Latitude varchar(50),
-	FOREIGN KEY (ID_Rota) REFERENCES Rota(ID_Rota)
+-- Table RoutePoints
+create table RoutePoints(
+    ID_RoutePoints SERIAL primary key,
+    ID_Route SERIAL,
+    Longitude varchar(50),
+    Latitude varchar(50),
+    FOREIGN KEY (ID_Route) REFERENCES Route(ID_Route)
 );
