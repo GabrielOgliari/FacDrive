4b73560ea1af8c2c7088de7a0cd8c6a83aa7ea4d
GabrielOgliari <125907742+GabrielOgliari@users.noreply.github.com>
2024-08-17 08:45:54 -0300

    Update User_db.js

--- Back/Cruds_DB/User_db.js
+++ Back/Cruds_DB/User_db.js
@@ -2,34 +2,36 @@
 class CRUDUser {
   constructor(pool) {
     this.pool = pool;
-    this.tableName = 'Users';
+    this.tableName = 'users';
   }
 
   // Create - Inserir um novo usuário
   async create(data) {
     try {
-      // console.log(data);
+      console.log(data);
       const query = `
         INSERT INTO ${this.tableName} 
-        (CPF, Registration, Name, Surname, BirthDate, DriverLicense, Phone, UserType, institutionalEmail, Password) 
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) 
+        (cpf, registration, name, surname, birthDate, driverLicense, phone, isDriver, institutionalEmail, password, gender) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) 
         RETURNING *`;
       const values = [
-        data.CPF,
-        data.Registration, 
-        data.Name,
-        data.Surname,
-        data.BirthDate,
-        data.DriverLicense,
-        data.Phone,
-        data.UserType,
+        data.cpf,
+        data.registration, 
+        data.name,
+        data.surname,
+        data.birthDate,
+        data.driverLicense,
+        data.phone,
+        data.isDriver,
         data.institutionalEmail,
-        data.Password
+        data.password,
+        data.gender
       ];
       const res = await this.pool.query(query, values);
       // const user = await this.readIdUser(data.Name, data.Surname);
       // console.log("O ID do user:",user);
       // return user, res.rows[0];
+      console.log(res.rows[0])
       return res.rows[0]; 
     } catch (error) {
       throw error;
@@ -41,7 +43,7 @@ class CRUDUser {
     console.log("Nome:",name);
     console.log("Sobrenome:",surname);
     try {
-      const query = `SELECT id_user FROM ${this.tableName} WHERE Name = $1 AND Surname = $2`;
+      const query = `SELECT idUser FROM ${this.tableName} WHERE name = $1 AND surname = $2`;
       const res = await this.pool.query(query, [name, surname]);
       console.log("Resposta:",res.rows[0].id_user);
       return res.rows[0].id_user;
@@ -54,7 +56,7 @@ class CRUDUser {
   async read(id) {
     try {
       if (id) {
-        const query = `SELECT * FROM ${this.tableName} WHERE ID_User = $1`;
+        const query = `SELECT * FROM ${this.tableName} WHERE idUser = $1`;
         const res = await this.pool.query(query, [id]);
         return res.rows[0];
       } else {
@@ -73,7 +75,7 @@ class CRUDUser {
       const fields = Object.keys(data).map((key, i) => `${key} = $${i + 1}`).join(', ');
       const values = Object.values(data);
 
-      const query = `UPDATE ${this.tableName} SET ${fields} WHERE ID_User = $${values.length + 1} RETURNING *`;
+      const query = `UPDATE ${this.tableName} SET ${fields} WHERE idUser = $${values.length + 1} RETURNING *`;
       const res = await this.pool.query(query, [...values, id]);
       return res.rows[0];
     } catch (error) {
@@ -84,7 +86,7 @@ class CRUDUser {
   // Delete - Remover um usuário específico
   async delete(id) {
     try {
-      const query = `DELETE FROM ${this.tableName} WHERE ID_User = $1 RETURNING *`;
+      const query = `DELETE FROM ${this.tableName} WHERE idUser = $1 RETURNING *`;
       const res = await this.pool.query(query, [id]);
       return res.rows[0];
     } catch (error) {
