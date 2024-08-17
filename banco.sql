dcb21fcdf68ef3b21486f80762d416b031f2553b
GabrielOgliari <125907742+GabrielOgliari@users.noreply.github.com>
2024-08-16 21:14:05 -0300

    feat: padronização do banco

--- banco.sql
+++ banco.sql
@@ -1,110 +1,111 @@
 -- Table User
-create table Users(
-    ID_User SERIAL primary key,
-    CPF varchar(14),
-    Registration varchar(9),
-    Name varchar(50),
-    Surname varchar(50),
-    BirthDate date,
-    DriverLicense varchar(11),
-    Phone varchar(17),
-    UserTypeMot BOOLEAN,
+create table users(
+    idUser SERIAL primary key,
+    cpf varchar(14),
+    registration varchar(9),
+    name varchar(50),
+    surname varchar(50),
+    birthDate date,
+    driverLicense varchar(11),
+    phone varchar(17),
+    isDriver BOOLEAN,
     institutionalEmail varchar(50),
-    Password varchar(50),
-    UserImage text
+    password varchar(50),
+    userImage text,
+    gender char
 );
 
 -- Table ClassDays
-create table ClassDays(
-    ID_Days SERIAL primary key,
-    ID_User SERIAL,
-    Monday BOOLEAN,
-    Tuesday BOOLEAN,
-    Wednesday BOOLEAN,
-    Thursday BOOLEAN,
-    Friday BOOLEAN,
-    Saturday BOOLEAN,
-    FOREIGN KEY (ID_User) REFERENCES Users(ID_User)
+create table classDays(
+    idDays SERIAL primary key,
+    idUser SERIAL,
+    monday BOOLEAN,
+    tuesday BOOLEAN,
+    wednesday BOOLEAN,
+    thursday BOOLEAN,
+    friday BOOLEAN,
+    saturday BOOLEAN,
+    FOREIGN KEY (idUser) REFERENCES users(idUser)
 );
 
 -- Table Address
-create table Address(
-    ID_Address SERIAL primary key,
-    ID_User SERIAL,
-    ZipCode varchar(9),
-    Street varchar(100),
-    Neighborhood varchar(50),
-    City varchar(50),
-    Number numeric(5),
-    AdditionalInfo varchar(50),
-    ReferencePoint varchar(50),
-    State varchar(50),
-    FOREIGN KEY (ID_User) REFERENCES Users(ID_User)
+create table address(
+    idAddress SERIAL primary key,
+    idUser SERIAL,
+    zipCode varchar(9),
+    street varchar(100),
+    neighborhood varchar(50),
+    city varchar(50),
+    number numeric(5),
+    additionalInfo varchar(50),
+    referencePoint varchar(50),
+    state varchar(50),
+    FOREIGN KEY (idUser) REFERENCES users(idUser)
 );
 
 -- Table vehicle
 create table vehicle(
-    ID_vehicle SERIAL primary key,
-    ID_User SERIAL,
-    ManufacturingDate Date,
-    ManufacturingYear Date,
-    ModelYear Date,
-    Color varchar(45),
-    Brand varchar(45),
-    Model varchar(45),
-    Plate varchar(7),
-		City varchar(45),
-		State varchar(45),
-    FOREIGN KEY (ID_User) REFERENCES Users(ID_User)
+    idVehicle SERIAL primary key,
+    idUser SERIAL,
+    manufacturingDate Date,
+    manufacturingYear Date,
+    modelYear Date,
+    color varchar(45),
+    brand varchar(45),
+    model varchar(45),
+    plate varchar(7),
+	city varchar(45),
+	state varchar(45),
+    FOREIGN KEY (idUser) REFERENCES users(idUser)
 );
 
 -- Table Route
-create table Route(
-    ID_Route SERIAL primary key,
-    ID_User SERIAL,
-    RouteName varchar(50),
-    FOREIGN KEY (ID_User) REFERENCES Users(ID_User)
+create table route(
+    idRoute SERIAL primary key,
+    idUser SERIAL,
+    routeName varchar(50),
+    FOREIGN KEY (idUser) REFERENCES users(idUser)
 );
 
 -- Table Relationship (Match)
-create table Relationship(
-    ID_Relationship SERIAL primary key,
-    Driver_ID SERIAL,
-    Rider_ID SERIAL,
-    Amount NUMERIC(10, 2),
-    FOREIGN KEY (Driver_ID) REFERENCES Users(ID_User),
-    FOREIGN KEY (Rider_ID) REFERENCES Users(ID_User)
+create table relationship(
+    idRelationship SERIAL primary key,
+    driverId SERIAL,
+    riderId SERIAL,
+    amount NUMERIC(10, 2),
+    FOREIGN KEY (driverId) REFERENCES users(idUser),
+    FOREIGN KEY (riderId) REFERENCES users(idUser)
 );
 
 -- Table Ride
-create table Ride(
-    ID_Ride SERIAL primary key,
-    ID_Relationship SERIAL,
-    DateOfRide date,
-    FOREIGN KEY (ID_Relationship) REFERENCES Relationship(ID_Relationship)
+create table ride(
+    idRide SERIAL primary key,
+    idRelationship SERIAL,
+    dateOfRide date,
+    FOREIGN KEY (idRelationship) REFERENCES relationship(idRelationship)
 );
 
 -- Table Booking
-create table Booking(
-    ID_Booking SERIAL primary key,
-    ID_Relationship SERIAL,
-    BookingDate date DEFAULT CURRENT_DATE,
-    FOREIGN KEY (ID_Relationship) REFERENCES Relationship(ID_Relationship)
+create table booking(
+    idBooking SERIAL primary key,
+    idRelationship SERIAL,
+    bookingDate date DEFAULT CURRENT_DATE,
+    FOREIGN KEY (idRelationship) REFERENCES relationship(idRelationship)
 );
 
 -- Table Debt
-create table Debt(
-    ID_Debt SERIAL primary key,
-    ID_Relationship SERIAL,
-    Amount NUMERIC(10, 2),
-    FOREIGN KEY (ID_Relationship) REFERENCES Relationship(ID_Relationship)
+create table debt(
+    idDebt SERIAL primary key,
+    idRelationship SERIAL,
+    amount NUMERIC(10, 2),
+    FOREIGN KEY (idRelationship) REFERENCES Relationship(idRelationship)
 );
 
 -- Table RoutePoints
-create table RoutePoints(
-    ID_RoutePoints SERIAL primary key,
-    ID_Route SERIAL,
-    Longitude varchar(50),
-    Latitude varchar(50),
-    FOREIGN KEY (ID_Route) REFERENCES Route(ID_Route)
+create table routePoints(
+    idRoutePoints SERIAL primary key,
+    idRoute SERIAL,
+    longitude varchar(50),
+    latitude varchar(50),
+    FOREIGN KEY (idRoute) REFERENCES route(idRoute)
 );
