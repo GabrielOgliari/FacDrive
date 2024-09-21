39d09dcc976fdba18b5cc258ef900cdf7649da3e
Matheus-Ei <t4igacomercial@gmail.com>
2024-09-20 21:40:11 -0300

    feat: front: make the basic style of the screen that sujests users

--- FacDriveAbexApp/src/screens/dashboard/components/RideOptions/index.tsx
+++ FacDriveAbexApp/src/screens/dashboard/components/RideOptions/index.tsx
@@ -1,15 +1,75 @@
+import Icon from 'react-native-vector-icons/AntDesign';
 import { HorisontalSeparator } from '../../../../components/UI/atoms/HorisontalSeparator';
 import * as S from './styles';
+import { useEffect, useState } from 'react';
 
 export const RideOptions = () => {
+    type userDataType = Array<{ name: string; days: number }>;
+
+    const [userData, setUserData] = useState([{}]);
+
+    const fetchUsers = () => {
+        const tempUserData: userDataType = [
+            {
+                name: 'Matheus Petri',
+                days: 5,
+            },
+            {
+                name: 'Evandro Jonas',
+                days: 2,
+            },
+            {
+                name: 'Lucas Silva',
+                days: 1,
+            },
+        ];
+
+        setUserData(tempUserData);
+    };
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    const acceptRide = () => {
+        console.log('accepted');
+    };
+
+    const rejectRide = () => {
+        console.log('rejected');
+    };
+
     return (
         <S.Body>
-            <S.User>
-                <S.Username>Matheus</S.Username>
-                <HorisontalSeparator space={10} />
+            <S.Title>Recomendações de caronas</S.Title>
+            {userData.map((item, index) => {
+                return (
+                    <S.User key={index}>
+                        <S.InfoView>
+                            <S.Username>{item.name}</S.Username>
+                            <HorisontalSeparator space={10} />
 
-                <S.CommunDays>1 dia</S.CommunDays>
-            </S.User>
+                            <S.CommunDays>
+                                {item.days} Dias em comum
+                            </S.CommunDays>
+                        </S.InfoView>
+
+                        <S.ButtonsView>
+                            <S.Button onPress={acceptRide}>
+                                <Icon
+                                    size={20}
+                                    color="green"
+                                    name="checkcircleo"
+                                />
+                            </S.Button>
+
+                            <S.Button onPress={rejectRide}>
+                                <Icon size={20} color="red" name="frowno" />
+                            </S.Button>
+                        </S.ButtonsView>
+                    </S.User>
+                );
+            })}
         </S.Body>
     );
 };
--- FacDriveAbexApp/src/screens/dashboard/components/RideOptions/styles.ts
+++ FacDriveAbexApp/src/screens/dashboard/components/RideOptions/styles.ts
@@ -1,26 +1,54 @@
-import styled from 'styled-components/native'
+import styled from 'styled-components/native';
 
 export const Body = styled.View`
     align-items: center;
-    justify-content: center;
-
-    background-color: rgba(0, 0, 0, 0.2);
-    height: 50px;
-
-    border-radius: 10px;
 `;
 
 export const User = styled.View`
     flex-direction: row;
-    gap: 20px;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-top: 8px;
+
+    height: 50px;
+    width: 100%;
+
+    border-radius: 15px;
+    border-color: rgba(0, 0, 0, 0.2);
+    border-width: 1px;
+    background-color: rgba(0, 0, 0, 0.04);
 `;
 
+export const InfoView = styled.View`
+    flex-direction: row;
+    align-items: center;
+
+    margin-left: 15px;
+`;
+
+export const ButtonsView = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+
+    margin-right: 15px;
+
+    width: 60px;
+`;
 export const Username = styled.Text`
-    color: black;
+    color: rgba(0, 0, 0, 0.6);
     font-weight: bold;
 `;
 
 export const CommunDays = styled.Text`
-    color: black;
-`
+    color: rgba(0, 0, 0, 0.6);
+    font-size: 12px;
+`;
 
+export const Button = styled.TouchableOpacity``;
+
+export const Title = styled.Text`
+    color: rgba(0, 0, 0, 0.8);
+    font-weight: bold;
+    font-size: 15px;
+`;
