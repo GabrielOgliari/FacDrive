a73c887fddef4ec6faabc40d4b7d74bf49e4aad9
GabrielOgliari <125907742+GabrielOgliari@users.noreply.github.com>
2024-09-28 14:16:43 -0300

    feat: teste para API

--- API/API.py
+++ API/API.py
@@ -38,4 +38,5 @@ def verificacao_veiculo():
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
-    app.run(debug=True, port=5000)
+    # app.run(debug=True, port=5000)
+    app.run()
