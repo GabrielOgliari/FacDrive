23cd2228b5b1ca2f3246727d1ead3714c4fec779
alexandrebeilner <alexandrebeilner8@gmail.com>
2024-11-02 12:33:02 -0300

    fix(Back-NodeJS): Change db connection.
    Refs: DB-CONN-FIX

--- Back/Banco/db.js
+++ Back/Banco/db.js
@@ -7,6 +7,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-export default pool; 
+export default pool;
